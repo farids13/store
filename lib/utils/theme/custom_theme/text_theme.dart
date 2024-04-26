b1294a1e7c7de53fd4fe0d31e898335ddb1f5857
Farid Satria <faridsatria24@gmail.com>
2024-04-26 16:11:00 +0700

    create util dart function

---
+++ lib/utils/theme/custom_theme/text_theme.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class TTextTheme {
+  static TextTheme lightTextTheme = TextTheme(
+    headlineLarge: const TextStyle().copyWith(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    headlineMedium: const TextStyle().copyWith(
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    headlineSmall: const TextStyle().copyWith(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    titleLarge: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    titleMedium: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    titleSmall: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    bodyLarge: const TextStyle().copyWith(
+      fontSize: 16.0,
+      color: Colors.black,
+    ),
+    bodyMedium: const TextStyle().copyWith(
+      fontSize: 14.0,
+      color: Colors.black,
+    ),
+    bodySmall: const TextStyle().copyWith(
+      fontSize: 12.0,
+      color: Colors.black,
+    ),
+  );
+
+  static TextTheme darkTextTheme = const TextTheme().copyWith(
+    headlineLarge: const TextStyle().copyWith(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    headlineMedium: const TextStyle().copyWith(
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    headlineSmall: const TextStyle().copyWith(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    titleLarge: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    titleMedium: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    titleSmall: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    bodyLarge: const TextStyle().copyWith(
+      fontSize: 16.0,
+      color: Colors.white,
+    ),
+    bodyMedium: const TextStyle().copyWith(
+      fontSize: 14.0,
+      color: Colors.white,
+    ),
+    bodySmall: const TextStyle().copyWith(
+      fontSize: 12.0,
+      color: Colors.white
+    ),
+  );
+}
